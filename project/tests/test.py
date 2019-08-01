14f6f76c4752b934713c74ce9e77852c1f5da3b5
Raoul Tjokrojoso <raoul@MacBook-Pro-van-Raoul-2.local>
2019-08-01 21:36:23 +0200

    add initial init test file

--- project/tests/test.py
+++ project/tests/test.py
@@ -37,12 +37,8 @@ class ProjectTests(unittest.TestCase):
  
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
-        self.assertIn(b'Welcome to the Kennedy Family Recipe App!', response.data)
-        self.assertIn(b'This site describes our favorite family recipes!', response.data)
-        self.assertIn(b'Breakfast Recipes', response.data)
-        self.assertIn(b'Lunch Recipes', response.data)
-        self.assertIn(b'Dinner Recipes', response.data)
-        self.assertIn(b'Dessert Recipes', response.data)
+        self.assertIn(b'Hi, Anouk!', response.data)
+     
  
  
 if __name__ == "__main__":
